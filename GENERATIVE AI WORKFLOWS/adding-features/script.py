35e125d8ddbcaa97413926637d40a38cf95d0605
ARBUCHELI <radio_seattle@hotmail.com>
2023-12-22 10:07:48 +0300

    adding features first part

---
+++ GENERATIVE AI WORKFLOWS/adding-features/script.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+# Connect to the MongoDB server
+client = MongoClient("mongodb://localhost:27017")
+
+# Access the database and collection
+db = client["company_database"]
+orders_collection = db["Orders"]
+
+# Change this line to find three orders
+most_recent_orders = orders_collection.find().sort("order_date", -1).limit(3)
+
+# Change these lines to print the three most recent order details
+for order in most_recent_orders:
+print("Most Recent Order:")
+print("Order ID:", most_recent_order["order_id"])
+print("Order Date:", most_recent_order["order_date"])
